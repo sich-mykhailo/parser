30013816f13b6f92c2f36b6f1e09e2e0fa4eb025
sich-mykhailo <sichmisha@gmail.com>
2023-05-07 14:37:15 +0300

    added docker and jenkins

--- src/test/java/com/parser/parser/ParserApplicationTests.java
+++ src/test/java/com/parser/parser/ParserApplicationTests.java
@@ -1,9 +1,7 @@
 package com.parser.parser;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ParserApplicationTests {
 
     @Test
